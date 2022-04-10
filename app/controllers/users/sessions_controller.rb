5b68d0542c8f035a63f3f9a1407e4ad06355881d
saijo-108 <toyatoyasj@gmail.com>
2022-04-10 18:31:43 +0900

    add:i18nを使って日本語化

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,16 +9,24 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
 
-  # protected
+  def failed
+    redirect_to root_path
+  end
+
+  protected
 
   def after_sign_in_path_for(resource)
     lists_path
@@ -27,6 +35,11 @@ class Users::SessionsController < Devise::SessionsController
   def after_sign_out_path_for(resource)
     root_path
   end
+
+  def auth_options
+    { scope: resource_name, recall: "#{controller_path}#failed" }
+  end
+
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
