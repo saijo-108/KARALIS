46782529a1c810e11d9d76ea798136b92cc0a93b
saijo-108 <toyatoyasj@gmail.com>
2022-05-26 23:28:29 +0900

    サービス・使用方法のスタイル #26

--- app/javascript/packs/lists/index.js
+++ app/javascript/packs/lists/index.js
@@ -1,4 +1,5 @@
 const swiper = new Swiper(".swiper", {
+    loop: true,
     pagination: {
         el: ".swiper-pagination"
       },
--- app/javascript/packs/top_pages/new.js
+++ app/javascript/packs/top_pages/new.js
@@ -5,3 +5,15 @@ const $target = $(".bg");
 setInterval(() => {
   $target.addClass(CLASSNAME);
 });
+
+const swiper = new Swiper(".swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction"
+  },
+  // ナビボタンが必要なら追加
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
