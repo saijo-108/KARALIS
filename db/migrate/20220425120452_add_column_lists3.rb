4952bd33e02c5108f21343af6968c6e9131735e8
saijo-108 <toyatoyasj@gmail.com>
2022-05-27 23:37:23 +0900

    残っていたマイグレーションファイルのsearchsong_idを削除

--- db/migrate/20220425120452_add_column_lists3.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnLists3 < ActiveRecord::Migration[5.2]
-  def change
-    add_column :lists, :searchsong_id, :string
-    add_column :lists, :preview, :string
-    add_column :lists, :song_title, :string
-    add_column :lists, :artist, :string
-  end
-end
