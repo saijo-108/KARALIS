4b17fcb068d58e65f86dac4048015564901e9e8b
saijo-108 <toyatoyasj@gmail.com>
2022-06-04 00:28:37 +0900

    rspotifyのidが必要ないので削除

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,8 +4,6 @@ class ListsController < ApplicationController
   require 'rspotify'
   require 'open-uri'
 
-  RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
-
   def new
     @list = current_user.lists.build
   end
