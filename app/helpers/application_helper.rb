34afa6b40c3af12b3116c19ea0f9aa6a23e16b61
saijo-108 <toyatoyasj@gmail.com>
2022-06-11 10:54:24 +0900

    三項演算子の修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
 
   def full_description(page_description = '')
     base_description = "KARALISは、自分の好きな曲でリストを作り仲間と共有できるアプリです。URLを共有するだけで好きな曲を共有できます。"
-    if page_description.empty? ? base_description : page_description
+    page_description.empty? ? base_description : page_description
   end
   
 end
