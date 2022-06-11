2e5ef5abab9580eae760419e788fa693145b8652
saijo-108 <toyatoyasj@gmail.com>
2022-06-11 10:37:37 +0900

    アナリティクスの修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
 
     page_title.empty? ? base_title : page_title + ' | ' + base_title
   end
+
+  def full_description(page_description = '')
+    base_description = "KARALISは、自分の好きな曲でリストを作り仲間と共有できるアプリです。URLを共有するだけで好きな曲を共有できます。"
+    if page_description.empty? ? base_description : page_description
+  end
+  
 end
