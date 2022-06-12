0d2c7c20e819d502597d31c8c29c9814c59fed0b
saijo-108 <toyatoyasj@gmail.com>
2022-06-12 22:53:23 +0900

    ユーザー新規登録エラー時のビューの崩れの修正とバリデーションのチェックのテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'Userモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    it "nameがない場合は登録できないこと" do
+      user = build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("を入力してください")
+    end
+
+    it "emailがない場合は登録できないこと" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+
+    it "passwordがない場合は登録できないこと" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+  end
 end
