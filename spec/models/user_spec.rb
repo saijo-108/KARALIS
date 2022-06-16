4cac3ef38cb38b073327c85795dd68b252c5e0ee
saijo-108 <toyatoyasj@gmail.com>
2022-06-16 16:31:54 +0900

    modelテスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Userモデルのテスト', type: :model do
+  before do
+    user = build(:user)
+  end
+
+  it "ユーザーネーム、メールアドレス、パスワードがある場合、有効である" do
+    user = build(:user)
+    expect(user).to be_valid
+  end
+
   describe 'バリデーションのテスト' do
     it "nameがない場合は登録できないこと" do
       user = build(:user, name: nil)
